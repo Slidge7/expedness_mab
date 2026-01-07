0e2f9f51c2ded62a0786df16dd972fa1bdecb78a
slidge7 <slidge7@gmail.com>
2026-01-07 17:25:49 +0100

    handle splash screen

--- App.tsx
+++ App.tsx
@@ -1,66 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useEffect } from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import BootSplash from 'react-native-bootsplash';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import {
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-  View,
-  Text,
-} from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks here...
+      // e.g., await checkUserAuth();
+      // e.g., await loadFonts();
+    };
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+    init().finally(async () => {
+      // Hide the splash screen with a fade out
+      await BootSplash.hide({ fade: true });
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View style={{ backgroundColor: 'red' }}>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-        <Text>In nnnnnn</Text>
-      </View>
-    </SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Welcome to Expedness</Text>
+    </SafeAreaView>
   );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: { fontSize: 24, fontWeight: 'bold' },
 });
 
 export default App;
